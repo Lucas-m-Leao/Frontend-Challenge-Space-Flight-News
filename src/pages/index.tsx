8c288c65cbda90f7ecaa52e41490415122cfdb3c
XxDarklucas xX <xxdarklucasxx@hotmail.com>
2022-11-13 21:35:02 -0300

    feat:creating folders

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Modal from '../components/modal'
+import { Container } from '../styles/modal'
+
+export default function Home() {
+    return (
+        <div>
+            <Container>
+                <h1> NextJS Blog with Butter CMS</h1>
+            </Container>
+        </div>
+    )
+}
